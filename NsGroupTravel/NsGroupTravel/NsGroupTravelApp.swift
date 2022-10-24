45ad02a456135feceb4d0b2842298f87adcca1e7
Omar <omar@Omars-MacBook-Pro.local>
2022-10-24 22:41:29 +0200

    firebase database setup and connected

---
+++ NsGroupTravel/NsGroupTravel/NsGroupTravelApp.swift
@@ -0,0 +1,22 @@
+//
+//  NsGroupTravelApp.swift
+//  NsGroupTravel
+//
+//  Created by Omar on 22/10/2022.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct NsGroupTravelApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
