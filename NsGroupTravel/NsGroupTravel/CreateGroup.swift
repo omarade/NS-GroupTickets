1ff1ba0e93c3a1b3e51b423c0e700c70a1b86164
Aciobăniţei,Alexandru A.C <a.aciobanitei@student.fontys.nl>
2022-11-07 03:10:11 +0000

    CreateGroup.swift

--- NsGroupTravel/NsGroupTravel/CreateGroup.swift
+++ NsGroupTravel/NsGroupTravel/CreateGroup.swift
@@ -171,26 +171,6 @@ struct CreateGroup: View {
         return temp
 
         }
-    
-    
-    
-        
-    
-    func loadJson(fromURLString urlString: String,
-                      completion: @escaping (Result<Data, Error>) -> Void) {
-    if let url = URL(string: urlString) {
-        let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                completion(.failure(error))
-            }
-            
-            if let data = data {
-                completion(.success(data))
-            }
-        }
-        urlSession.resume()
-    }
-}
         
         var body: some View {
             NavigationView{
